db6c640c433740e47d94a67149b055a3e1469a5a
Adriana Słabosz <ada@MacBook-Pro-Adriana.local>
2022-09-19 19:31:28 +0200

    Add Tip Amount Component

--- src/Components/TipAmount/index.js
+++ src/Components/TipAmount/index.js
@@ -1,5 +1,15 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Text, Img} from "@chakra-ui/react";
 
 export function TipAmount() {
-    return <Box fontFamily="Space Mono">Tip Amount</Box>
+    return  <Box  fontFamily="Space Mono" display="flex" flexDirection="row" height="46px">
+  <Box w="50%">
+  <Text fontSize="17px" color="white">Tip Amount</Text>
+        <Text fontSize="14px" color="#F4FAFA">/ person </Text>
+    </Box>
+  <Box w="50%" display="flex" flexDirection="row" alignItems="center">
+  <Img src="icon-dollar-green.svg" alt="Dollar sign" height="40px"/>
+    <Text fontSize="46px" color="#20A291">0.00</Text>
+    </Box>
+</Box>
+
 }
--- src/Components/index.js
+++ src/Components/index.js
@@ -1,4 +1,4 @@
-import { Container, Box} from "@chakra-ui/react"
+import { Box} from "@chakra-ui/react"
 import { BillInput } from "./BillInput"
 import { PeopleNumberInput } from "./PeopleNumberInput"
 import { ResetButton } from "./ResetButton"
@@ -7,16 +7,16 @@ import { TipSelect } from "./TipSelect"
 import { TotalAmount } from "./TotalAmount"
 
 export function SplitterComponent() {
-    return <Container bgColor="white" display="flex" flexDirection="row" height="400px" width="800px" p="0" borderRadius="20px">
+    return <Box width="850px" bgColor="white" display="flex" flexDirection="row" height="400px" p="0" borderRadius="20px">
         <Box width="50%" m="20px" p="20px">
 <BillInput/>
 <TipSelect/>
 <PeopleNumberInput/>
 </Box>
-<Box bgColor="#00494D" width="50%" m="20px" borderRadius="20px" p="20px">
+<Box bgColor="#00494D" width="50%" m="20px" borderRadius="20px" p="20px" display="flex" flexDirection="column" justifyContent="space-between">
 <TipAmount/>
 <TotalAmount/>
 <ResetButton/>
 </Box>
-    </Container>
+    </Box>
 }
