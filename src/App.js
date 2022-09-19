18090bf2d8336334eeaf3b165fe7d97a2b8da207
Adriana Słabosz <ada@MacBook-Pro-Adriana.local>
2022-09-19 12:19:42 +0200

    Install and Add Chakra UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { ChakraProvider, Box, Container} from '@chakra-ui/react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <ChakraProvider>
+<Box bgColor="#C5E4E7" height="100vh" display="flex" justifyContent="center" alignItems="center">
+  <Container bgColor="white">
+  </Container>
+</Box>
+  </ChakraProvider>
 }
 
 export default App;
