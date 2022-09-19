a446b0ff8a00adfe98108a334ecc2aeb13dd6a8c
Adriana Słabosz <ada@MacBook-Pro-Adriana.local>
2022-09-19 22:37:49 +0200

    Add Number of People Input

--- src/Components/PeopleNumberInput/index.js
+++ src/Components/PeopleNumberInput/index.js
@@ -1,5 +1,19 @@
-import { Box } from "@chakra-ui/react";
+import { Box , Heading, Input, InputGroup, InputLeftElement} from "@chakra-ui/react";
+import { personSign } from "../Images";
 
 export function PeopleNumberInput() {
-    return <Box fontFamily="Space Mono">Number of People</Box>
+    return <Box fontFamily="Space Mono">
+    <Heading fontFamily="Space Mono" fontSize="14px" color="#5E7A7D" pb="10px">
+        Number of People
+        </Heading> 
+        <InputGroup>
+        <InputLeftElement
+      pointerEvents='none'
+      children={personSign}
+    />
+        <Input variant="filled" color="#7F9C9F"
+_placeholder={{ opacity: 0.4}} focusBorderColor="crimson"
+        backgroundColor="#F4FAFA"  fontSize="24px" placeholder="0" textAlign="end"/>
+        </InputGroup>
+        </Box>
 }
