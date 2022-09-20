4a8867c5d5767cef46cedeca6f7c2fc63bd69d64
Adriana Słabosz <ada@MacBook-Pro-Adriana.local>
2022-09-20 10:40:53 +0200

    Add Tip % Buttons

--- src/Components/TipSelect/index.js
+++ src/Components/TipSelect/index.js
@@ -1,5 +1,17 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Heading, Button, ButtonGroup , Input  } from "@chakra-ui/react";
 
 export function TipSelect() {
-    return <Box fontFamily="Space Mono">How much TIP</Box>
+    return <Box>
+        <Heading fontFamily="Space Mono" fontSize="14px" color="#5E7A7D">Select Tip %</Heading>
+        <ButtonGroup fontFamily="Space Mono" width="100%" paddingBlock="10px">
+        <Button width="33%" bgColor="#00494D" color="white" fontSize="24px" _hover={{bgColor:"#9fe8df", color:"#00494D"}}>5%</Button>
+        <Button width="33%" bgColor="#00494D" color="white" fontSize="24px" _hover={{bgColor:"#9fe8df", color:"#00494D"}}>10%</Button>
+        <Button width="33%" bgColor="#00494D" color="white" fontSize="24px" _hover={{bgColor:"#9fe8df", color:"#00494D"}}>15%</Button>
+        </ButtonGroup>
+        <ButtonGroup fontFamily="Space Mono" width="100%" >
+        <Button width="33%" bgColor="#00494D" color="white" fontSize="24px" _hover={{bgColor:"#9fe8df", color:"#00494D"}}>25%</Button>
+        <Button width="33%" bgColor="#00494D" color="white" fontSize="24px" _hover={{bgColor:"#9fe8df", color:"#00494D"}}>50%</Button>
+        <Input width="33%" bgColor="#F4FAFA" color="#00494D" fontSize="20px" placeholder="Custom" variant="filled" focusBorderColor="#26C0AB" textAlign="end" _placeholder={{color:"#7F9C9F"}}/>
+        </ButtonGroup>
+    </Box>
 }
