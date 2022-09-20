6dd39a3734251c58dafb78165d603f9bf14c94a1
Adriana Słabosz <ada@MacBook-Pro-Adriana.local>
2022-09-20 13:06:47 +0200

    Add Per Person Calculation

--- src/Components/PeopleNumberInput/index.js
+++ src/Components/PeopleNumberInput/index.js
@@ -1,7 +1,12 @@
 import { Box , Heading, Input, InputGroup, InputLeftElement} from "@chakra-ui/react";
 import { personSign } from "../Images";
 
-export function PeopleNumberInput() {
+export function PeopleNumberInput({setPeopleNumber}) {
+
+  const handleChange = event => {
+    setPeopleNumber(event.target.value);
+  }
+
     return <Box fontFamily="Space Mono">
     <Heading fontFamily="Space Mono" fontSize="14px" color="#5E7A7D" pb="10px">
         Number of People
@@ -12,8 +17,10 @@ export function PeopleNumberInput() {
       children={personSign}
     />
         <Input variant="filled" color="#7F9C9F"
-_placeholder={{ opacity: 0.4}} focusBorderColor="crimson"
-        backgroundColor="#F4FAFA"  fontSize="24px" placeholder="0" textAlign="end"/>
+_placeholder={{ opacity: 0.4}} focusBorderColor="#26C0AB"
+        backgroundColor="#F4FAFA"  fontSize="24px" placeholder="0" textAlign="end"
+        onChange={handleChange}
+        />
         </InputGroup>
         </Box>
 }
--- src/Components/index.js
+++ src/Components/index.js
@@ -7,23 +7,26 @@ import { TipSelect } from "./TipSelect";
 import { TotalAmount } from "./TotalAmount";
 import { useState } from "react";
 import { TotalAmountCalculator } from "./TipCalculator";
+import { TipAmountCalculator } from "./TipCalculator";
 
 export function SplitterComponent() {
 
   const [bill, setBill] = useState("") 
   const [tipPercentage, setTipPercentage] = useState("")
-  
-  const result = TotalAmountCalculator({bill, tipPercentage})
+  const [peopleNumber, setPeopleNumber] = useState("")
+
+  const totalResult = TotalAmountCalculator({bill, tipPercentage, peopleNumber})
+  const totalTip =  TipAmountCalculator({bill, tipPercentage, peopleNumber}) 
     
     return <Box width="850px" bgColor="white" display="flex" flexDirection="row" height="400px" p="0" borderRadius="20px">
         <Box width="50%" m="20px" p="20px" display="flex" flexDirection="column" justifyContent="space-between">
 <BillInput setBill={setBill}/>
 <TipSelect setTipPercentage={setTipPercentage}/>
-<PeopleNumberInput/>
+<PeopleNumberInput setPeopleNumber={setPeopleNumber}/>
 </Box>
 <Box bgColor="#00494D" width="50%" m="20px" borderRadius="20px" p="40px" display="flex" flexDirection="column" justifyContent="space-between">
-<TipAmount bill={bill} tipPercentage={tipPercentage}/>
-<TotalAmount total={result}/>
+<TipAmount total={totalTip} bill={bill} tipPercentage={tipPercentage}/>
+<TotalAmount total={totalResult}/>
 <ResetButton/>
 </Box>
     </Box>
